46921843258ed903987bf2f8664c14445ea47f09
Nopall <ahmad.noval2705@gmail.com>
2022-05-20 09:52:08 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+
+require 'vendor/autoload.php';
+// reference the Dompdf namespace
+use Dompdf\Dompdf;
+
+// instantiate and use the dompdf class
+$dompdf = new Dompdf();
+$dompdf->loadHtml(file_get_contents('ppdb/formulirppdb.html'));
+
+// (Optional) Setup the paper size and orientation
+$dompdf->setPaper('A4', 'landscape');
+
+// Render the HTML as PDF
+$dompdf->render();
+
+// Output the generated PDF to Browser
+// $dompdf->stream();
+
+$output = $dompdf->output();
+file_put_contents('Brochure.pdf', $output);
